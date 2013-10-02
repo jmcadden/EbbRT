fea2540c00b9656cd60ee52fe92ffdfe7bb43cb9
Jim Cadden <jmcadden@bu.edu>
2013-10-02 14:26:43 -0400

    Static EbbIds are bare-metal only

--- src/ebb/Timer/Timer.hpp
+++ src/ebb/Timer/Timer.hpp
@@ -29,8 +29,10 @@ namespace ebbrt {
   protected:
     Timer(EbbId id) : EbbRep{id} {}
   };
+#ifdef __ebbrt__
   const EbbRef<Timer> timer =
     EbbRef<Timer>(lrt::trans::find_static_ebb_id("Timer"));
+#endif
 }
 
 #endif
